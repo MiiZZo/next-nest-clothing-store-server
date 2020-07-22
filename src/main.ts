723ca67e5d1273a4c9b79c5025e785b717ab87ce
MiiZZo <miizzo.js@mail.ru>
2020-07-22 16:16:53 +0300

    udpate: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: config.get('CLIENT_URL')
+  });
+  await app.listen(5000);
 }
 bootstrap();
