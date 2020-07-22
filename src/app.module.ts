f6f23b2e827c7996c5c172db05e4c0fd58e0a631
MiiZZo <miizzo.js@mail.ru>
2020-07-22 15:48:00 +0300

    upload project

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0-obnfg.mongodb.net/${process.env.DB_NAME}`,
+      {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
+  ],
+})
+export class AppModule {}
