f1001e9d777fe36d1690e5d0dc1ff0163b62e315
MiiZZo <miizzo.js@mail.ru>
2020-07-22 17:07:19 +0300

    fix: add products module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ProductsModule,
     UsersModule,
     AuthModule,
     ConfigModule.forRoot(),
